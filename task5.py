858b47f121cbb5e08661ab97c6b702f2133c54f7
PalMorthy <115100210+PalMorthy@users.noreply.github.com>
2022-11-10 02:29:58 +0300

    refactor(task5): imported strings

--- task5.py
+++ task5.py
@@ -1,10 +1,11 @@
 from random import sample
+from string import digits, ascii_uppercase, ascii_lowercase
 
 
 def get_random_password(password_size: int = 8) -> str:
-    __alphabet_lower__ = 'abcdefghigjklmnopqrstuvwxyz'
-    __alphabet_upper__ = __alphabet_lower__.upper()
-    __numbers__ = "0123456789"
+    __alphabet_lower__ = ascii_lowercase
+    __alphabet_upper__ = ascii_uppercase
+    __numbers__ = digits
     string_sample = ''.join(__alphabet_lower__ + __alphabet_upper__ + __numbers__)
     list_sample = [item for item in string_sample]
     password = ''.join(sample(list_sample, password_size))
